1bec6683f04eff0a3e016cb926d22aa4e268d844
soohykim <24tngus@naver.com>
2023-07-28 00:33:21 +0900

    1. string

--- 인프런 알고리즘/String1_8.java
+++ 인프런 알고리즘/String1_8.java
@@ -6,6 +6,34 @@ public class String1_8 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        String str = in.nextLine().toUpperCase();
+        String[] arr = str.split("");
+        String[] result = new String[arr.length];
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int c = (int) str.charAt(i);
+            if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+                result[count++] = arr[i];
+            }
+        }
+        for (int i = 0; i < count / 2; i++) {
+            int len = count - 1;
+            char c1 = result[i].charAt(0);
+            char c2 = result[len - i].charAt(0);
+            if (isSameChar(c1, c2) != 1) {
+                System.out.println("NO");
+                return ;
+            }
+        }
+        System.out.println("YES");
         in.close();
+    }
+
+    public static int isSameChar(char c1, char c2) {
+        if (c1 == c2)
+            return (1);
+        else
+            return (0);
     }
 }
